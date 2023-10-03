e9f002637bf06826ec4d1ae723705a3db186c2bc
ionutdev <iariton@deloittece.com>
2023-10-03 16:21:41 +0300

    update-code-with-x

--- scriptList.js
+++ scriptList.js
@@ -1,24 +1,25 @@
-//  localStorage
+
+
+// localStorage
 function loadTasks() {
   const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   const list = document.getElementById("list");
 
   tasks.forEach((taskText) => {
-    const li = document.createElement("li");
-    li.textContent = taskText;
+    const li = createTaskElement(taskText); // Create task element with "X" button
     list.appendChild(li);
   });
 }
 
-//  task and save  localStorage
+// task and save to localStorage
 function addTask() {
   const valueAdd = document.getElementById("add").value;
-  if (!valueAdd) return; // Don't add empty tasks
+  if (!valueAdd) return alert("Please add a value"); // Don't add empty tasks
 
-  const li = document.createElement("li");
-  li.textContent = valueAdd;
+  const list = document.getElementById("list");
+  const li = createTaskElement(valueAdd);
 
-  document.getElementById("list").appendChild(li);
+  list.appendChild(li);
 
   const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   tasks.push(valueAdd);
@@ -27,6 +28,23 @@ function addTask() {
   document.getElementById("add").value = "";
 }
 
+// remove task
+function removeTask(text) {
+  const list = document.getElementById("list");
+  const listItems = list.getElementsByTagName("li");
+
+  for (let i = 0; i < listItems.length; i++) {
+    if (listItems[i].textContent === text) {
+      list.removeChild(listItems[i]);
+      const tasks = JSON.parse(localStorage.getItem("tasks"));
+      const updateTask = tasks.filter((x) => x !== text);
+      localStorage.setItem("tasks", JSON.stringify(updateTask));
+      break;
+    }
+  }
+}
+
+// Event listeners
 window.addEventListener("load", () => {
   loadTasks();
 });
@@ -35,7 +53,7 @@ document.getElementById("add-button").addEventListener("click", () => {
   addTask();
 });
 
-//  dark mode
+// dark mode
 function toggleDarkMode() {
   const container = document.querySelector(".container");
   container.classList.toggle("dark-mode");
@@ -43,3 +61,26 @@ function toggleDarkMode() {
   const isDarkMode = container.classList.contains("dark-mode");
   localStorage.setItem("dark-mode", isDarkMode);
 }
+
+// x- button
+function createTaskElement(taskText) {
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "X";
+  removeButton.addEventListener("click", () => {
+    removeTask(taskText);
+  });
+
+  li.appendChild(removeButton);
+  return li;
+}
+// remove
+document.getElementById("list").addEventListener("click", function (event) {
+  if (event.target.tagName === "BUTTON") {
+    const li = event.target.parentElement;
+    const text = li.textContent.trim(); 
+    removeTask(text);
+  }
+});
